09b9de3b0e17d6dc6b88105233a5ad7b05ffc484
mshenhera <mshenhera@users.noreply.github.com>
2020-02-12 15:19:45 +0200

    Set default log level for mongodriver to INFO

--- lib/mosql/cli.rb
+++ lib/mosql/cli.rb
@@ -115,8 +115,10 @@ module MoSQL
       log.outputters = Log4r::StdoutOutputter.new(STDERR)
       if options[:verbose] >= 1
         log.level = Log4r::DEBUG
+        Mongo::Logger.logger.level = Logger::DEBUG
       else
         log.level = Log4r::INFO
+        Mongo::Logger.logger.level = Logger::INFO
       end
     end
 
